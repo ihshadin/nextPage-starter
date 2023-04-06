27c4d93025be07d96a7988d4ea4e1a0bd0fecb36
IH Shadin <shadinfr@yahoo.com>
2023-04-06 14:41:55 +0600

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className='App'>
-      <h1>Welcome to nextPage!</h1>
+      <h1 className="text-2xl font-bold">Welcome to nextPage!</h1>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App></App>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+)
