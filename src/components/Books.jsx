4b285e971d9d74559c5bc1d8ec7457f4425c4cbf
IH Shadin <shadinfr@yahoo.com>
2023-04-09 07:03:18 +0600

    error page added

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useLoaderData, useNavigation } from 'react-router-dom';
+import Book from './Book';
+import LoaderSpinner from './LoaderSpinner';
 
 const Books = () => {
+    // Loader Spinner
+    const loading = useNavigation();
+    if (loading.state === 'loading') {
+        return <LoaderSpinner />
+    }
+
+    const { books } = useLoaderData();
+    // console.log(books);
     return (
-        <div>
-            <h2>This is Books page</h2>
+        <div className='my-container'>
+            <div className='grid gap-6 mb-8 lg:grid-cols-4 xm:grid-cols-2'>
+                {
+                    books.map(book => <Book key={book.isbn13} book={book} />)
+                }
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,14 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Books from './components/Books'
+import BookDetails from './components/BookDetails'
+import ErrorPage from './components/ErrorPage'
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: '/',
@@ -18,7 +21,13 @@ const router = createBrowserRouter([
             },
             {
                 path: 'books',
-                element: <Books />
+                element: <Books />,
+                loader: () => fetch('https://api.itbook.store/1.0/new')
+            },
+            {
+                path: 'book/:id',
+                element: <BookDetails />,
+                loader: ({ params }) => fetch(`https://api.itbook.store/1.0/books/${params.id}`)
             },
             {
                 path: 'about',
